890d6c3fca5adeb24db6e5b24869d5bf95f70ba3
abdulkadir-dev0 <abdulkadir.ucrr@gmail.com>
2025-04-27 14:29:44 +0300

    Add files via upload

---
+++ Odev1.kt
@@ -0,0 +1,58 @@
+package com.example.abdulkadir.kotlindersleri.degiskenler
+
+// Ödev 1
+
+fun main() {
+
+    //Değişken Tanımlamaları
+    val sehir: String             = "Erzurum"
+    val ulke: String              = "Türkiye"
+    val telefon: String           = "+90 537 123 4567"
+    val postaKodu: String         = "25000"
+    val email: String             = "ogrenci@uni.edu.tr"
+    val meslek: String            = "Öğrenci"
+    val stokMiktari: Int          = 10250
+    val musteriAdi: String        = "Ali Veli"
+    val bakiye: Double            = 235.75
+    val dogumGunu: String         = "17/12/2000"
+    val maas: Double              = 34500.00
+    val medeniDurum: Boolean      = false
+    val urunYorum: String         = "Çok kaliteli ve kullanışlı"
+
+    val odemeTarihi: String       = "25/04/2025"
+    val odeme: Double             = 999.99
+    val siparisAdedi: Int         = 13
+    val arabaModeli: String       = "Toyota Corolla"
+    val kitapAdi: String          = "Kotlin ile Programlama"
+    val yayinlamaTarihi: String   = "10/01/2022"
+    val indirimMiktari: Double    = 15.0
+    val odaSayisi: Int            = 124
+    val enlem: Double             = 40.7128
+    val boylam: Double            = 14.0060
+    val urunAdi: String           = "Kablosuz Kulaklık"
+    val yemekFiyati: Double       = 115.50
+
+    val marka: String             = "Samsung"
+    val muzikAdi: String          = "Kara Sevda"
+    val videoSuresi: Int          = 210
+    val urunPuani: Double         = 4.7
+    val resimAdi: String          = "Manzara"
+    val dosyaFormati: String      = "jpg"
+    val renk: String              = "Mavi"
+    val renkKodu: String          = "#0000FF"
+    val telefonModeli: String     = "Galaxy S21"
+    val ekranBoyutu: Double       = 6.2
+    val agirlik: Double           = 5.165
+
+    val ulusalGun: String         = "23 Nisan Ulusal Egemenlik ve Çocuk Bayramı"
+    val tatilGunu: String         = "Ramazan Bayramı"
+    val rezervasyonTarihi: String = "27/04/2025"
+    val sokakAdi: String          = "Atatürk Caddesi"
+    val otobusHatti: String       = "22T"
+    val kalanDakika: Int          = 45
+    val takipKodu: String         = "TRX123456789"
+    val kuponSuresi: Int          = 7
+    val kuponKodu: String         = "INDIRIM20"
+    val faturaAdresi: String      = "Öğrenci Yurdu, Balıkesir"
+
+}
